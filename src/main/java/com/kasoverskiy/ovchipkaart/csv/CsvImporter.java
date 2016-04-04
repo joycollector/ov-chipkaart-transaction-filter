5778bd6e2190b337efce71e7f7d070da4e776053
VadimKomyshenets <komyshenets@mail.ru>
2016-04-04 16:09:01 +0300

    Holidays for the period. Refactoring: replaced the class Date to the class Calendar and use Predicate

--- src/main/java/com/kasoverskiy/ovchipkaart/csv/CsvImporter.java
+++ src/main/java/com/kasoverskiy/ovchipkaart/csv/CsvImporter.java
@@ -6,6 +6,7 @@ import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.*;
+import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,15 +14,10 @@ import java.util.regex.Pattern;
  * Created by Вадим on 03.04.2016.
  */
 public class CsvImporter {
-    private  Set<Date> holidays = new HashSet<Date>();
-
-    public  List<Transaction> importCsv(InputStream is) throws IOException {
+    public List<Transaction> importCsv(InputStream is) throws IOException {
         List<Transaction> result = new ArrayList<Transaction>();
-
-        getHolidays();
-
-        GregorianCalendar checkIn;
-        GregorianCalendar checkOut;
+        Calendar checkIn;
+        Calendar checkOut;
         String[] record = new String[10]; //one record in CSV
         int y, m, d, h, min;
         double amounth;
@@ -38,14 +34,9 @@ public class CsvImporter {
                 h = Integer.parseInt(record[3].split(":")[0]);
                 min = Integer.parseInt(record[3].split(":")[1]);
                 checkOut = new GregorianCalendar(y, m, d, h, min);
-
-                if (!isValid(checkOut))
-                    continue;
-
                 amounth = Double.valueOf(record[5]);
                 result.add(new Transaction(
-                        checkIn.getTime(), record[2],
-                        checkOut.getTime(), record[4],
+                        checkIn, record[2], checkOut, record[4],
                         amounth, record[6], record[7], record[8], record[9]));
 
             } catch (NumberFormatException e) {
@@ -54,52 +45,74 @@ public class CsvImporter {
                 //wrong record
             }
         }
+        //Adding holidays for all years for which there is a transaction
+        //На тот случай, если отчётный период захватывает 2 и более смежных годов
+        Comparator<Transaction> yearTransaction = new Comparator<Transaction>() {
+            @Override
+            public int compare(Transaction o1, Transaction o2) {
+                return o1.getCheckOut().get(Calendar.YEAR) - o2.getCheckOut().get(Calendar.YEAR);
+            }
+        };
+        final Set<Calendar> holidays = new HashSet<>();
+        int beginYear = Collections.min(result, yearTransaction).getCheckOut().get(Calendar.YEAR);
+        int endYear = Collections.max(result, yearTransaction).getCheckOut().get(Calendar.YEAR);
+        for (int i = beginYear; i <= endYear; i++) {
+            holidays.addAll(getHolidays(i));
+        }
+        //Remove holidays and other unpaid date
+        result.removeIf(new Predicate<Transaction>() {
+            @Override
+            public boolean test(Transaction transaction) {
+                if (transaction.getCheckOut().get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY ||
+                        transaction.getCheckOut().get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY ||
+                        transaction.getCheckOut().get(Calendar.HOUR_OF_DAY) >= 18 ||
+                        holidays.contains(transaction.getCheckIn())) {
+//                    System.out.println("del "+transaction);
+                    return true;
+                }
+                return false;
+            }
+        });
 
         return result;
     }
 
-    private  void getHolidays() throws IOException {
-        int currentYear = new GregorianCalendar().get(Calendar.YEAR);
+    /**
+     * Return set national NL holidays for the year from holidayapi.com
+     *Тут, наверняка, надо использовать JSON но я просто распарсил
+     * @param currentYear
+     * @return
+     * @throws IOException
+     */
+    public Set<Calendar> getHolidays(int currentYear) throws IOException {
         URL url = new URL("http://holidayapi.com/v1/holidays?country=NL&year=" + currentYear);
         URLConnection connection = url.openConnection();
         BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String data = reader.readLine();
         Pattern pattern = Pattern.compile("\\d{4}-\\d{2}-\\d{2}");
-        Matcher matcher = pattern.matcher(data);
+        Matcher matcher = pattern.matcher(reader.readLine());
+
+        Set<Calendar> result = new HashSet<Calendar>();
         while (matcher.find()) {
             int y = Integer.parseInt(matcher.group().split("-")[0]);
             int m = Integer.parseInt(matcher.group().split("-")[1]) - 1;
             int d = Integer.parseInt(matcher.group().split("-")[2]);
-            holidays.add(new GregorianCalendar(y, m, d).getTime());
+            result.add(new GregorianCalendar(y, m, d));
         }
-//        for (Date date : holidays) {
-//            System.out.println(date);
+        return result;
+    }
+
+
+//    public static void main(String[] args) {
+//        try {
+//            List<Transaction> a = new CsvImporter().importCsv(new FileInputStream(new File("src/test/resources/transactions_example.csv")));
+//            for (Transaction transaction : a) {
+//                System.out.println(transaction);
+//            }
+//            System.out.println(a.size());
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
 //        }
-    }
-
-    private  boolean isValid(GregorianCalendar date) {
-        Date contain = new GregorianCalendar(date.get(Calendar.YEAR), date.get(Calendar.MONTH), date.get(Calendar.DATE)).getTime();
-        if (
-                date.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY ||
-                        date.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY ||
-                        date.get(Calendar.HOUR_OF_DAY) >= 18 ||
-                        holidays.contains(contain)) {
-            return false;
-        }
-        return true;
-    }
-
-    public static void main(String[] args) {
-        try {
-            List<Transaction> a = new CsvImporter().importCsv(new FileInputStream(new File("src/test/resources/transactions_example.csv")));
-            for (Transaction transaction : a) {
-                System.out.println(transaction);
-            }
-            System.out.println(a.size());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+//    }
 }

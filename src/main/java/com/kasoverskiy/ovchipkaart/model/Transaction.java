9a0869d9bdc5cdd6f351970c8d48d82c98d7864b
VadimKomyshenets <komyshenets@mail.ru>
2016-04-05 18:41:14 +0300

    Holidays for the period. Refactoring: replaced the class Date to the class Calendar and use Predicate

--- src/main/java/com/kasoverskiy/ovchipkaart/model/Transaction.java
+++ src/main/java/com/kasoverskiy/ovchipkaart/model/Transaction.java
@@ -1,18 +1,16 @@
 package com.kasoverskiy.ovchipkaart.model;
 
-import javax.annotation.Nullable;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Created by Вадим on 03.04.2016.
  */
 public class Transaction {
-    private Calendar checkIn;
+    private LocalDateTime checkIn;
     private String departure;
-    private Calendar checkOut;
-
-
+    private LocalDateTime checkOut;
     private String destination;
     private double amount;
     private String transaction;
@@ -20,22 +18,19 @@ public class Transaction {
     private String product;
     private String comments;
 
-    public Transaction(Calendar checkIn, String departure,
-                       Calendar checkOut, String destination,
-                       double amount, String transaction,
-                       @Nullable String classTrans, @Nullable String comments, @Nullable String product) {
-        this.checkIn = checkIn;
-        this.departure = departure;
-        this.checkOut = checkOut;
-        this.destination = destination;
-        this.amount = amount;
-        this.transaction = transaction;
-        this.classTrans = classTrans;
-        this.comments = comments;
-        this.product = product;
+    private Transaction(Builder builder) {
+        this.checkIn = builder.checkIn;
+        this.departure = builder.departure;
+        this.checkOut = builder.checkOut;
+        this.destination = builder.destination;
+        this.amount = builder.amount;
+        this.transaction = builder.transaction;
+        this.classTrans = builder.classTrans;
+        this.comments = builder.comments;
+        this.product = builder.product;
     }
 
-    public Calendar getCheckIn() {
+    public LocalDateTime getCheckIn() {
         return checkIn;
     }
 
@@ -43,7 +38,7 @@ public class Transaction {
         return departure;
     }
 
-    public Calendar getCheckOut() {
+    public LocalDateTime getCheckOut() {
         return checkOut;
     }
 
@@ -59,6 +54,10 @@ public class Transaction {
         return transaction;
     }
 
+    public String getClassTrans() {
+        return classTrans;
+    }
+
     public String getProduct() {
         return product;
     }
@@ -72,6 +71,70 @@ public class Transaction {
         SimpleDateFormat start = new SimpleDateFormat("E yyyy/MM/dd");
         SimpleDateFormat end = new SimpleDateFormat("HH:mm");
 //        return (checkIn) + " " + departure + " -> " + (checkOut) + " " + destination;
-        return start.format(checkIn.getTime()) + " " + departure + " -> " + end.format(checkOut.getTime()) + " " + destination;
+        return checkIn.format(DateTimeFormatter.ofPattern("E dd-MM-yyyy")) + " " + departure + " -> " +
+                checkOut.format(DateTimeFormatter.ofPattern("HH:mm")) + " " + destination;
+    }
+
+    static public class Builder {
+
+        private LocalDateTime checkIn;
+        private String departure;
+        private LocalDateTime checkOut;
+        private String destination;
+        private double amount;
+        private String transaction;
+        private String classTrans;
+        private String product;
+        private String comments;
+
+        public Transaction build() {
+            return new Transaction(this);
+        }
+
+        public Builder checkIn(LocalDateTime checkIn) {
+            this.checkIn = checkIn;
+            return this;
+        }
+
+        public Builder departure(String departure) {
+            this.departure = departure;
+            return this;
+        }
+
+        public Builder checkOut(LocalDateTime checkOut) {
+            this.checkOut = checkOut;
+            return this;
+        }
+
+        public Builder destination(String destination) {
+            this.destination = destination;
+            return this;
+        }
+
+        public Builder amount(double amount) {
+            this.amount = amount;
+            return this;
+        }
+
+        public Builder transaction(String transaction) {
+            this.transaction = transaction;
+            return this;
+        }
+
+        public Builder classTrans(String classTrans) {
+            this.classTrans = classTrans;
+            return this;
+        }
+
+        public Builder product(String product) {
+            this.product = product;
+            return this;
+        }
+
+        public Builder comments(String comments) {
+            this.comments = comments;
+            return this;
+        }
+
     }
 }

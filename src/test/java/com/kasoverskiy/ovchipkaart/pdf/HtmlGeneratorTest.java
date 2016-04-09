501217b4cbac8fc8bf0d9b60167dfdadc076e86d
joycollector@gmail.com <joycollector@gmail.com>
2016-04-09 14:19:15 +0200

    Html & Pdf generation

---
+++ src/test/java/com/kasoverskiy/ovchipkaart/pdf/HtmlGeneratorTest.java
@@ -0,0 +1,14 @@
+package com.kasoverskiy.ovchipkaart.pdf;
+
+import org.junit.Test;
+
+/**
+ * Created by joycollector on 4/7/16.
+ */
+public class HtmlGeneratorTest {
+
+    @Test
+    public void testCreateHtml() throws Exception {
+
+    }
+}

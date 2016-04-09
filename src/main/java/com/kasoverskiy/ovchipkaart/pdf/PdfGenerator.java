251533009e93d2e1c8d2dad43da148c2a8c806cb
joycollector@gmail.com <joycollector@gmail.com>
2016-04-09 14:19:15 +0200

    Html & Pdf generation

--- src/main/java/com/kasoverskiy/ovchipkaart/pdf/PdfGenerator.java
+++ src/main/java/com/kasoverskiy/ovchipkaart/pdf/PdfGenerator.java
@@ -4,16 +4,23 @@ import com.kasoverskiy.ovchipkaart.OvException;
 import com.lowagie.text.DocumentException;
 import org.w3c.dom.Document;
 import org.xhtmlrenderer.pdf.ITextRenderer;
+import org.xhtmlrenderer.resource.XMLResource;
+import org.xml.sax.InputSource;
 
-import java.io.OutputStream;
+import java.io.*;
 
 /**
  * Created by joycollector on 4/4/16.
  */
 public class PdfGenerator {
 
-    public void createPdf(Document document, OutputStream os) {
+    /**
+     * @param is source HTML
+     * @param os resulting PDF
+     */
+    public void createPdf(InputStream is, OutputStream os) {
         try {
+            Document document = XMLResource.load(is).getDocument();
             ITextRenderer renderer = new ITextRenderer();
             renderer.setDocument(document, null);
             renderer.layout();
